d879c8f9baf140860d728ee4222b6fe0d42b4ac8
Sighmox <Sighmox@github.com>
2019-04-02 19:38:27 -0500

    Chinese Study

---
+++ Chinese Study/Char.swift
@@ -0,0 +1,17 @@
+//
+//  Char.swift
+//  Chinese Study
+//
+//  Created by Paul Baker on 4/2/19.
+//  Copyright © 2019 Paul Baker. All rights reserved.
+//
+
+import Foundation
+
+struct Char {
+    
+    let chineseCharacter: String
+    let pinyin: String
+    let englishTranslation: String
+    
+}
---
+++ Chinese Study/CharacterCell.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterCell.swift
+//  Chinese Study
+//
+//  Created by Paul Baker on 4/2/19.
+//  Copyright © 2019 Paul Baker. All rights reserved.
+//
+
+import UIKit
+
+class CharacterCell: UICollectionViewCell {
+    
+    @IBOutlet var characterLabel: UILabel!
+}
